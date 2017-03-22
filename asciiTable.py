4a5a9f55c523fcb4bf5cc48eb60dda1bd52394fb
jc432592 <graham.ruttiman@my.jcu.edu.au>
2017-03-22 15:41:39 +1000

    22/3,3.38pm

--- asciiTable.py
+++ asciiTable.py
@@ -1,10 +1,5 @@
-
-
-#lower = 33
-#upper = 127
-#function for asking for a number between two limits122
-
 def get_number(lower, upper):
+    """"Function to input number within upper and lower limits"""
     valid_number = False
     while valid_number == False:
         try:
@@ -23,13 +18,7 @@ def get_number(lower, upper):
         else:
             valid_number = True
     return number
-#get_number(number,lower,upper)
-#min_number = 33
-#max_number = 127
-#number = int(input("Enter a number between {} and {}:".format(min_number,max_number)))
-#while number < min_number or number > max_number:
-#    print("The number must be BETWEEN {} and {}".format(min_number, max_number))
-#    number = int(input("Enter a number between {} and {}:".format(min_number, max_number)))
+
 character = input("Enter a character:")
 print("The ASCII code for",character,"is",ord(character))
 num = get_number(33,127)
