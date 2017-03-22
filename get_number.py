822d1d7883bdde1fe6c57c9568a0c72381864460
jc432592 <graham.ruttiman@my.jcu.edu.au>
2017-03-22 13:00:00 +1000

    22/3 function creation 2

--- get_number.py
+++ get_number.py
@@ -9,6 +9,8 @@ def get_number(lower, upper):
         print("Please entert a valid number")
     number = int(input("Enter a number between {} and {}:".format(lower, upper)))
     while number < lower or number > upper:
+        print ("The number must be BETWEEN {} and {}".format(lower, upper)))
         return number = int(input("Enter a number between {} and {}:".format(lower, upper)))
+    return True
 get_number(lower,upper)
 
