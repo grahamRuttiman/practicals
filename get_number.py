66a0b3a5b166c9d0f3902fc3327c75bcd5286bf7
jc432592 <graham.ruttiman@my.jcu.edu.au>
2017-03-22 14:18:36 +1000

    22/3,2.05

--- get_number.py
+++ get_number.py
@@ -9,14 +9,16 @@ def get_number(lower, upper):
         except ValueError:
            print("Please enter a valid number")
            number = int(input("Enter a number between {} and {}:".format(lower, upper)))
-        except:
-            if number < lower or number > upper:
-                print("The number must be BETWEEN {} and {}".format(lower, upper))
+
+        while number < lower or number > upper:
+            print("The number must be BETWEEN {} and {}".format(lower, upper))
+            try:
+                number = int(input("Enter a number between {} and {}:".format(lower, upper)))
+            except ValueError:
+                print("Please enter a valid number")
                 number = int(input("Enter a number between {} and {}:".format(lower, upper)))
         else:
             valid_number = True
-    #number = int(input("Enter a number between {} and {}:".format(lower, upper)))
-
     return True
 get_number(lower,upper)
 
